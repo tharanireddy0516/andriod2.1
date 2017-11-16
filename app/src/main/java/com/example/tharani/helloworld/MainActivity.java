166d1211e2ad376331c541ee8763a352ad49a294
Tharani <tharani.reddychelumalla05@gmail,com>
2017-11-16 15:55:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tharani/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tharani.helloworld;/**/
+/*libraries imported for writing the code
+* AppCompatActivity is base class for activities
+* Bundle handles the orientation of the activity
+*/
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+   /*onCreate is the first method in the life cycle of an activity
+   * setContentView is used to set layout for the activity
+   *R is a resource*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
